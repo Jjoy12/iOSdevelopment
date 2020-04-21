bf5796cca6ebfb6093df24516625a8923960ac3b
Joy <jerrionjoy@gmail.com>
2020-04-21 13:04:01 -0400

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Jerrion Joy on 4/21/20.
+//  Copyright © 2020 Jerrion Joy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
